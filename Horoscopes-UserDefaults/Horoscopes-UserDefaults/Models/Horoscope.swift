58d2524977ff4632822a33b7e2f6b81b73a1cc55
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-09-24 14:45:51 -0400

    adds Horoscope struct and file

---
+++ Horoscopes-UserDefaults/Horoscopes-UserDefaults/Models/Horoscope.swift
@@ -0,0 +1,16 @@
+//
+//  Horoscope.swift
+//  Horoscopes-UserDefaults
+//
+//  Created by Sunni Tang on 9/24/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import Foundation
+
+struct Horoscope {
+    let sunsign: String
+    let date: String
+    let horoscope: String
+    let mood: String
+}
