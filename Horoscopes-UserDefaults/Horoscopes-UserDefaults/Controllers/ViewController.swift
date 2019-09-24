cbfa384f315fbe5523e1c29d579ba7775f8a41c5
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-09-24 17:28:48 -0400

    loads UserDefault elements

--- Horoscopes-UserDefaults/Horoscopes-UserDefaults/Controllers/ViewController.swift
+++ Horoscopes-UserDefaults/Horoscopes-UserDefaults/Controllers/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: - IB Outlets
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var sunsignLabel: UILabel!
     @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var horoscopeLabel: UILabel!
+    
+    //MARK: - Variables
+    var horoscope: Horoscope?
     
     var user: User? {
         didSet {
@@ -22,31 +26,33 @@ class ViewController: UIViewController {
         }
     }
     
-    var horoscope: Horoscope?
-    
     var name: String = "" {
         didSet {
             welcomeLabel.text = "Welcome " + self.name + "!"
         }
     }
     
-    
+    //MARK: - Lifecycle functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        nameTextField.delegate = self
+        configureTextField()
         configureDatePicker()
-        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadHoroscopeData()
         if let storedName = UserDefaults.standard.value(forKey: "name") as? String {
             name = storedName
         }
         if let storedBirthday = UserDefaults.standard.value(forKey: "birthday") as? Date {
             datePicker.date = storedBirthday
         }
-        
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        loadHoroscopeData()
+        if let storedSunsign = UserDefaults.standard.value(forKey: "sunsign") as? String {
+            sunsignLabel.text = storedSunsign
+        }
+        if let storedHoroscope = UserDefaults.standard.value(forKey: "horoscope") as? String {
+            horoscopeLabel.text = storedHoroscope
+        }
         
     }
 
@@ -55,8 +61,13 @@ class ViewController: UIViewController {
         user = User(name: name, birthday: datePicker.date)
         UserDefaults.standard.set(user?.name, forKey: "name")
         UserDefaults.standard.set(user?.birthday, forKey: "birthday")
+        UserDefaults.standard.set(self.horoscope?.sunsign, forKey: "sunsign")
+        UserDefaults.standard.set(self.horoscope?.horoscope, forKey: "horoscope")
     }
     
+    private func configureTextField() {
+        nameTextField.delegate = self
+    }
     
     private func configureDatePicker() {
         datePicker.datePickerMode = .date
@@ -65,7 +76,7 @@ class ViewController: UIViewController {
     private func loadHoroscopeData() {
         if let userSign = user?.sunsign {
             let urlStr = "http://sandipbgt.com/theastrologer/api/horoscope/\( userSign)/today/"
-            print(urlStr)
+
             HoroscopeAPIClient.manager.getHoroscopeFromURL(urlStr: urlStr ) { (result) in
                 DispatchQueue.main.async {
                     switch result {
@@ -75,7 +86,8 @@ class ViewController: UIViewController {
                         self.horoscope = horoscopeFromOnline
                         self.sunsignLabel.text = self.horoscope?.sunsign
                         self.horoscopeLabel.text = self.horoscope?.horoscope
-                        
+//                        UserDefaults.standard.set(self.horoscope?.sunsign, forKey: "sunsign")
+//                        UserDefaults.standard.set(self.horoscope?.horoscope, forKey: "horoscope")
                         
                     }
                 }
