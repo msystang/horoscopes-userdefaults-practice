3b1a2a4f1ff43691cdf8d6e1f4d89676bd236ba0
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-09-24 15:05:47 -0400

    adds User model struct

---
+++ Horoscopes-UserDefaults/Horoscopes-UserDefaults/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Horoscopes-UserDefaults
+//
+//  Created by Sunni Tang on 9/24/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let birthday: Date
+}
